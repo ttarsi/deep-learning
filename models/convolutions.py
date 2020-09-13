0f34560bc8cf7a90114d19846dbf0ff5a5625fdf
tytarsi <tytarsi@gmail.com>
2020-09-12 21:33:37 -0700

    convolutional nets implementations

---
+++ models/convolutions.py
@@ -0,0 +1,337 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+# Simple 2D convolutions
+def corr2d(X, K):
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = torch.sum((X[i: i + h, j: j + w] * K))
+    return Y
+
+
+class Conv2D(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(kernel_size))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        return corr2d(x, self.weight) + self.bias
+
+X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+K = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+print(f"X:\n{X}")
+print(f"K:\n{K}")
+print(f"corr2d:\n{corr2d(X, K)}")
+
+X = torch.ones((6, 8))
+X[:, 2:6] = 0
+K = torch.tensor([[1.0, -1.0]])
+Y = corr2d(X, K)
+print(f"X:\n{X}")
+print(f"K:\n{K}")
+print(f"Y:\n{Y}")
+print(f"Y using X.T:\n{corr2d(torch.transpose(X, 0, 1), K)}")
+
+print("-------------------------------------------")
+
+# Learning the Kernel
+conv2d = nn.Conv2d(1,1, kernel_size=(1, 2), bias=False)
+X = X.reshape((1, 1, 6, 8))
+Y = Y.reshape((1, 1, 6, 7))
+
+print("learning the kernel")
+for i in range(10):
+    Y_hat = conv2d(X)
+    l = (Y_hat - Y) ** 2
+    conv2d.zero_grad()
+    l.sum().backward()
+    # Update the kernel
+    conv2d.weight.data[:] -= 3e-2 * conv2d.weight.grad
+    if (i + 1) % 2 == 0:
+        print(f'batch {i+1}, loss {l.sum():.3f}')
+print(conv2d.weight.data[:])
+
+print("-------------------------------------------")
+
+# Padding
+def comp_conv2d(conv2d, X):
+    X = X.reshape((1,1) + X.shape)
+    Y = conv2d(X)
+    return Y.reshape(Y.shape[2:])
+
+conv2d = nn.Conv2d(1, 1, kernel_size=3, padding=1)
+X = torch.rand(size=(8, 8))
+print(f"X:\n{X}")
+print(f"conv2d:\n{conv2d}")
+print(f"output shape:\n{comp_conv2d(conv2d, X).shape}")
+
+conv2d = nn.Conv2d(1, 1, kernel_size=(5, 3), padding=(2, 1))
+print()
+print(f"conv2d:\n{conv2d}")
+print(f"output shape:\n{comp_conv2d(conv2d, X).shape}")
+
+# Padding and stride
+conv2d = nn.Conv2d(1, 1, kernel_size=3, padding=1, stride=2)
+print()
+print(f"conv2d:\n{conv2d}")
+print(f"output shape:\n{comp_conv2d(conv2d, X).shape}")
+
+conv2d = nn.Conv2d(1, 1, kernel_size=(3, 5), padding=(0, 1), stride=(3, 4))
+print()
+print(f"conv2d:\n{conv2d}")
+print(f"output shape:\n{comp_conv2d(conv2d, X).shape}")
+
+print("-------------------------------------------")
+
+# Multi input channels
+def corr2d_multi_in(X, K):
+    return sum(corr2d(x, k) for x, k in zip(X, K))
+
+X = torch.tensor([[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
+               [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]])
+K = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]]])
+print(f"X:\n{X}")
+print(f"K:\n{K}")
+print(f"corr2d_multi_in:\n{corr2d_multi_in(X, K)}")
+
+# Multi input and output channels
+def corr2d_multi_in_out(X, K):
+    return torch.stack([corr2d_multi_in(X, k) for k in K], 0)
+
+print(f"K:\n{K.shape}")
+K = torch.stack((K, K + 1, K + 2), 0)
+print("K = torch.stack((K, K + 1, K + 2), 0)")
+print(f"K:\n{K.shape}")
+
+print(f"corr2d_multi_in_out(X, K):\n {corr2d_multi_in_out(X, K)}")
+
+print("-------------------------------------------")
+
+# 1x1 convolution across input channels
+def corr2d_multi_in_out_1x1(X, K):
+    c_i, h, w = X.shape
+    c_o = K.shape[0]
+    X = X.reshape((c_i, h * w))
+    K = K.reshape((c_o, c_i))
+    Y = torch.matmul(K, X)
+    return Y.reshape((c_o, h, w))
+
+X = torch.normal(0, 1, (3, 3, 3))
+K = torch.normal(0, 1, (2, 3, 1, 1))
+
+Y1 = corr2d_multi_in_out_1x1(X, K)
+Y2 = corr2d_multi_in_out(X, K)
+
+print(f"X:\n{X}")
+print(f"K:\n{K}")
+print(f"Y1:\n{Y1}")
+print(f"Y2:\n{Y2}")
+
+assert(float(torch.sum(torch.abs(Y1 - Y2))) < 1e-6)
+print("-------------------------------------------")
+
+
+# Pooling layers
+def pool2d(X, pool_size, mode='max'):
+    p_h, p_w = pool_size
+    Y = torch.zeros((X.shape[0] - p_h + 1, X.shape[1] - p_w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            if mode == 'max':
+                Y[i, j] = X[i: i + p_h, j: j + p_w].max()
+            elif mode == 'avg':
+                Y[i, j] = X[i: i + p_h, j: j + p_w].mean()
+    return Y
+
+X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+print(f"X:\n{X}")
+print(f"pool2d(X, (2,2)):\n{pool2d(X, (2, 2))}")
+print(f"pool2d(X, (2, 2), 'avg'):\n{pool2d(X, (2, 2), 'avg')}")
+
+X = torch.reshape(torch.arange(16, dtype=torch.float32), (1, 1, 4, 4))
+print(f"X:\n{X}")
+
+pool2d = nn.MaxPool2d(3)
+print(f"pool2d:\n{pool2d(X)}")
+
+pool2d = nn.MaxPool2d(3, padding=1, stride=2)
+print(f"pool2d p=1, s=2:\n{pool2d(X)}")
+
+
+pool2d = nn.MaxPool2d((2,3), padding=(1,1), stride=(2,3))
+print(f"pool2d (2,3), (1,1), (2,3):\n{pool2d(X)}")
+
+X = torch.cat((X, X + 1), 1)
+print(f"X:\n{X}")
+pool2d = nn.MaxPool2d(3, padding=1, stride=2)
+print(f"pool2d:\n{pool2d(X)}")
+print("-------------------------------------------")
+
+# Convolutioal Neural Networks
+class Reshape(torch.nn.Module):
+    def forward(self, x):
+        return x.view(-1,1,28,28)
+
+net = torch.nn.Sequential(
+    Reshape(),
+    nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Conv2d(6, 16, kernel_size=5), nn.Sigmoid(),
+    nn.AvgPool2d(kernel_size=2, stride=2),
+    nn.Flatten(),
+    nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
+    nn.Linear(120, 84), nn.Sigmoid(),
+    nn.Linear(84, 10))
+
+print("Conv neural net")
+X = torch.randn(size=(1, 1, 28, 28), dtype=torch.float32)
+for layer in net:
+    X = layer(X)
+    print(layer.__class__.__name__,'output shape: \t',X.shape)
+
+print("-------------------------------------------")
+print("train")
+
+def evaluate_accuracy_gpu(net, data_iter, device=None): #@save
+    net.eval()
+    if not device:
+        device = next(iter(net.parameters())).device
+    metric = d2l.Accumulator(2)
+    for X, y in data_iter:
+        X, y = X.to(device), y.to(device)
+        metric.add(d2l.accuracy(net(X), y), d2l.size(y))
+    return metric[0] / metric[1]
+
+def train_func(net, train_iter, test_iter, num_epochs, lr,
+              device=d2l.try_gpu()):
+    def init_weights(m):
+        if type(m) == nn.Linear or type(m) == nn.Conv2d:
+            torch.nn.init.xavier_uniform_(m.weight)
+    net.apply(init_weights)
+    print('training on', device)
+    net.to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+    loss = nn.CrossEntropyLoss()
+    timer = d2l.Timer()
+    for epoch in range(num_epochs):
+        metric = d2l.Accumulator(3)
+        for i, (X, y) in enumerate(train_iter):
+            timer.start()
+            net.train()
+            optimizer.zero_grad()
+            X, y = X.to(device), y.to(device)
+            y_hat = net(X)
+            l = loss(y_hat, y)
+            l.backward()
+            optimizer.step()
+            with torch.no_grad():
+                metric.add(l * X.shape[0], d2l.accuracy(y_hat, y), X.shape[0])
+            timer.stop()
+            train_loss = metric[0]/metric[2]
+            train_acc = metric[1]/metric[2]
+            if (i + 1) % 50 == 0:
+                print(f"epoch: {epoch} --- iter: {i} --- of {len(train_iter)}")
+                print(f"train loss: {train_loss} --- train acc: {train_acc}")
+        test_acc = evaluate_accuracy_gpu(net, test_iter)
+    print(f'loss {train_loss:.3f}, train acc {train_acc:.3f}, '
+          f'test acc {test_acc:.3f}')
+    print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
+          f'on {str(device)}')
+
+print("train")
+batch_size = 256
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size=batch_size)
+lr, num_epochs = 0.9, 2
+# train_func(net, train_iter, test_iter, num_epochs, lr)
+
+
+print("-------------------------------------------")
+print("alex net modifications")
+
+net = nn.Sequential(
+    nn.Conv2d(1, 96, kernel_size=11, stride=4, padding=1), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
+    nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
+    nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2),
+    nn.Flatten(),
+    nn.Linear(6400, 4096), nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(4096, 4096), nn.ReLU(),
+    nn.Dropout(p=0.5),
+    nn.Linear(4096, 10))
+
+X = torch.randn(1, 1, 224, 224)
+for layer in net:
+    X=layer(X)
+    print(layer.__class__.__name__,'Output shape:\t',X.shape)
+
+batch_size = 128
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+
+lr, num_epochs = 0.01, 2 
+# train_func(net, train_iter, test_iter, num_epochs, lr)
+print("\"train\"")
+
+
+print("-------------------------------------------")
+print("VGG modifications")
+
+def vgg_block(num_convs, in_channels, out_channels):
+    layers=[]
+    for _ in range(num_convs):
+        layers.append(nn.Conv2d(in_channels, out_channels,
+                                kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+        in_channels = out_channels
+    layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+    return nn.Sequential(*layers)
+
+conv_arch = ((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))
+
+def vgg(conv_arch):
+    conv_blks=[]
+    in_channels=1
+    for (num_convs, out_channels) in conv_arch:
+        conv_blks.append(vgg_block(num_convs, in_channels, out_channels))
+        in_channels = out_channels
+
+    return nn.Sequential(
+        *conv_blks, nn.Flatten(),
+        nn.Linear(out_channels * 7 * 7, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 10))
+
+net = vgg(conv_arch)
+
+X = torch.randn(size=(1, 1, 224, 224))
+for blk in net:
+    X = blk(X)
+    print(blk.__class__.__name__,'output shape:\t',X.shape)
+
+ratio = 4
+small_conv_arch = [(pair[0], pair[1] // ratio) for pair in conv_arch]
+net = vgg(small_conv_arch)
+
+lr, num_epochs, batch_size = 0.05, 10, 128
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
+# train_func(net, train_iter, test_iter, num_epochs, lr)
+print("\"train\"")
+
+
+
+
+
+
+
+
+
