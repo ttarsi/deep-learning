cf51683166be8e0c1886ff4e9fbf4809d1839607
tytarsi <tytarsi@gmail.com>
2020-09-13 11:25:25 -0700

    dense nets

--- models/convolutions.py
+++ models/convolutions.py
@@ -497,5 +497,77 @@ lr, num_epochs, batch_size = 0.05, 10, 256
 # train_func(net, train_iter, test_iter, num_epochs, lr)
 
 
+print("-------------------------------------------")
+print("Dense Net")
+
+def conv_block(input_channels, num_channels):
+    return nn.Sequential(
+        nn.BatchNorm2d(input_channels), 
+        nn.ReLU(),
+        nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1))
+
+
+class DenseBlock(nn.Module):
+    def __init__(self, num_convs, input_channels, num_channels):
+        super(DenseBlock, self).__init__()
+        layer = []
+        for i in range(num_convs):
+            layer.append(conv_block(
+                num_channels * i + input_channels, num_channels))
+        self.net = nn.Sequential(*layer)
+
+    def forward(self, X):
+        for blk in self.net:
+            Y = blk(X)
+            X = torch.cat((X, Y), dim=1)
+        return X
+
+blk = DenseBlock(2, 3, 10)
+X = torch.randn(4, 3, 8, 8)
+Y = blk(X)
+print(blk)
+print(f"X: {X}")
+print(f"Y.shape: {Y.shape}")
+
+def transition_block(input_channels, num_channels):
+    return nn.Sequential(
+        nn.BatchNorm2d(input_channels), 
+	nn.ReLU(),
+        nn.Conv2d(input_channels, num_channels, kernel_size=1),
+        nn.AvgPool2d(kernel_size=2, stride=2))
+
+blk = transition_block(23, 10)
+print(blk)
+print(f"blk(Y): {blk(Y).shape}")
+
+# Dense Net model
+b1 = nn.Sequential(
+    nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3),
+    nn.BatchNorm2d(64), nn.ReLU(),
+    nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+
+num_channels, growth_rate = 64, 32
+num_convs_in_dense_blocks = [4, 4, 4, 4]
+blks = []
+for i, num_convs in enumerate(num_convs_in_dense_blocks):
+    blks.append(DenseBlock(num_convs, num_channels, growth_rate))
+    num_channels += num_convs * growth_rate
+    if i != len(num_convs_in_dense_blocks) - 1:
+        blks.append(transition_block(num_channels, num_channels // 2))
+        num_channels = num_channels // 2
+
+net = nn.Sequential(
+    b1, *blks,
+    nn.BatchNorm2d(num_channels), nn.ReLU(),
+    nn.AdaptiveMaxPool2d((1, 1)),
+    nn.Flatten(),
+    nn.Linear(num_channels, 10))
+
+lr, num_epochs, batch_size = 0.1, 10, 256
+# train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=96)
+# train_func(net, train_iter, test_iter, num_epochs, lr)
+
+
+
 
 
